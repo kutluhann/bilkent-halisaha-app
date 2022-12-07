f2d6bbd7ec91be09925633274668750b8ab65557
Kaanayden <kaanayden15@gmail.com>
2022-12-07 23:56:23 +0300

    Remove unneccessary methods

--- app/src/main/java/com/example/bilkenthalisahaapp/Firestore.java
+++ app/src/main/java/com/example/bilkenthalisahaapp/Firestore.java
@@ -26,59 +26,7 @@ public class Firestore {
         db.collection("matches").document( time + "" ).set(match);
 
     }
-
-    public static void getNewMatches() {
-        final int PAGE_LIMIT = 1;
-            FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-            Query query;
-            if (matches.isEmpty() ) {
-                query = db.collection("matches")
-                        .orderBy("time", Query.Direction.ASCENDING)
-                        .limit(PAGE_LIMIT);
-            } else {
-                Match lastMatch = matches.get(matches.size() - 1);
-                query = db.collection("matches")
-                        .orderBy("time", Query.Direction.ASCENDING)
-                        .startAfter(lastMatch.getTime())
-                        .limit(PAGE_LIMIT);
-            }
-
-            query.addSnapshotListener(new EventListener<QuerySnapshot>() {
-                @Override
-                public void onEvent(@Nullable QuerySnapshot value,
-                                    @Nullable FirebaseFirestoreException e) {
-
-                    if (e != null) {
-                        return;
-                    }
-
-                    for (QueryDocumentSnapshot doc : value) {
-                        if(doc!=null && doc.exists()) {
-                            Match newMatch = doc.toObject(Match.class);
-
-                            int indexOf = indexOfMatch(newMatch);
-                            if (indexOf != -1) {
-                                matches.set(indexOf, newMatch);
-                            } else {
-                                matches.add(newMatch);
-                            }
-                        }
-
-
-                    }
-
-                }
-            });
-
-    }
-
-    public static int indexOfMatch(Match match) {
-        for(int i = 0; i < matches.size(); i++) {
-            if( matches.get(i).equals(match) ) return i;
-        }
-        return -1;
-    }
+    
 
     public static ArrayList<Match> getMatches() {
         return matches;
