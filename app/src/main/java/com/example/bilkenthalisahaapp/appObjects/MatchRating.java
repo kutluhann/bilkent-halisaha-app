7dd0c4d55422469b33c9e2218a6701377cf6bedb
Furkan Emre Kolukırık <emre.kolukirik@ug.bilkent.edu.tr>
2022-12-11 23:33:41 +0300

    Add matchRating class

--- app/src/main/java/com/example/bilkenthalisahaapp/appObjects/MatchRating.java
+++ app/src/main/java/com/example/bilkenthalisahaapp/appObjects/MatchRating.java
@@ -1,4 +1,26 @@
 package com.example.bilkenthalisahaapp.appObjects;
 
+import java.util.HashMap;
+
 public class MatchRating {
+
+    private double averageRating;
+    private HashMap<String,Integer> givenRatingsByPlayer;
+
+    public MatchRating(){
+        givenRatingsByPlayer = new HashMap<String,Integer>();
+    }
+
+    public void getRatingGivenByPlayer(){
+        //There may not be need for that method.
+    }
+
+    public double calculateRating(){
+        for(int temp: givenRatingsByPlayer.values()){
+            averageRating += temp;
+        }
+        averageRating= averageRating/ givenRatingsByPlayer.values().size();
+        return averageRating;
+    }
+
 }
