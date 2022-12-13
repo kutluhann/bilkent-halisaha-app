790747201caa3513a125b64d2f5a3d7f70f10745
kutluhann <kutluhan.sipka@ug.bilkent.edu.tr>
2022-12-13 20:52:44 +0300

    Change addMatch class to enable add match

--- app/src/main/java/com/example/bilkenthalisahaapp/AddMatch.java
+++ app/src/main/java/com/example/bilkenthalisahaapp/AddMatch.java
@@ -2,32 +2,40 @@ package com.example.bilkenthalisahaapp;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.bilkenthalisahaapp.appObjects.Location;
 import com.example.bilkenthalisahaapp.appObjects.Match;
 import com.example.bilkenthalisahaapp.databinding.FragmentAddMatchBinding;
 import com.google.firebase.Timestamp;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.TimeZone;
-import java.util.UUID;
 
 
-public class AddMatch extends Fragment {
+public class AddMatch extends Fragment implements AdapterView.OnItemSelectedListener {
 
     private FragmentAddMatchBinding binding;
     private int mYear, mMonth, mDay, mHour, mMinute;
+
+    private String location, time, playerCount, position;
+
+    final Calendar c = Calendar.getInstance();
+
 
     @Override
     public View onCreateView(
@@ -42,46 +50,44 @@ public class AddMatch extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(getContext(),
-                R.array.locations_array, android.R.layout.simple_spinner_item);
+        initializeSpinnerAdapters();
 
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Set the text of datePicket to current date
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
 
-        binding.locationSpinner.setAdapter(arrayAdapter);
+        binding.datePicker.setText(getFormattedDate(mDay, mMonth + 1, mYear));
 
-        binding.addButton.setOnClickListener(new View.OnClickListener() {
+        binding.createButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                String locationName = binding.editLocationName.getText().toString();
-
-                int teamSize = Integer.parseInt( binding.editTextTeamSize.getText().toString() );
-                int maxTeamSize = Integer.parseInt( binding.editTextMaxTeamSize.getText().toString() );
                 Calendar cal = Calendar.getInstance();
 
-                //binding.textViewDate.setText(cal.getTime().toString() + " " + cal.getTimeZone());
-                //cal.set(mYear, mMonth, mDay, mHour, mMinute, 0);
-                //only hour
-                cal.set(mYear, mMonth, mDay, mHour, 0, 0);
+                int indexOfDot = time.indexOf(".");
+                int hour = Integer.parseInt(time.substring(0, indexOfDot));
+
+                cal.set(mYear, mMonth, mDay, hour, 0, 0);
                 cal.setTimeZone(TimeZone.getTimeZone("Europe/Istanbul" ));
-                //binding.textViewTime.setText(cal.getTime().toString() + " " + cal.getTimeZone());
                 Timestamp timestamp = new Timestamp( cal.getTime() );
 
-                                                                                        // We should datermine how to specify the UUID.
-                Match newMatch = new Match(locationName, timestamp, teamSize, maxTeamSize);
+                int totalPlayerCount = Integer.parseInt(playerCount) * 2;
+
+                Match newMatch = new Match(location, timestamp, 0, totalPlayerCount);
 
                 Firestore.updateMatch(newMatch);
-                }
+
+                Toast.makeText(getContext(),"Match is created successfully", Toast.LENGTH_SHORT).show();
+
+                NavHostFragment.findNavController(AddMatch.this)
+                        .navigate(R.id.matches_navigation);
+            }
             });
 
             binding.datePicker.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    final Calendar c = Calendar.getInstance();
-                    mYear = c.get(Calendar.YEAR);
-                    mMonth = c.get(Calendar.MONTH);
-                    mDay = c.get(Calendar.DAY_OF_MONTH);
-
                     DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(),
                         new DatePickerDialog.OnDateSetListener() {
 
@@ -89,7 +95,7 @@ public class AddMatch extends Fragment {
                             public void onDateSet(DatePicker view, int year,
                                                   int monthOfYear, int dayOfMonth) {
 
-                                binding.datePicker.setText(dayOfMonth + "." + (monthOfYear + 1) + "." + year);
+                                binding.datePicker.setText(getFormattedDate(dayOfMonth, monthOfYear + 1, year));
                                 mYear = year;
                                 mMonth = monthOfYear;
                                 mDay = dayOfMonth;
@@ -98,33 +104,6 @@ public class AddMatch extends Fragment {
                     datePickerDialog.show();
                 }
             });
-
-        binding.selectTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Get Current Time
-                final Calendar c = Calendar.getInstance();
-                mHour = c.get(Calendar.HOUR_OF_DAY);
-                mMinute = c.get(Calendar.MINUTE);
-
-                // Launch Time Picker Dialog
-                TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(),
-                        new TimePickerDialog.OnTimeSetListener() {
-
-                            @Override
-                            public void onTimeSet(TimePicker view, int hourOfDay,
-                                                  int minute) {
-
-                                binding.textViewTime.setText(hourOfDay + ":" + minute);
-                                mHour = hourOfDay;
-                                mMinute = minute;
-                            }
-                        }, mHour, mMinute, true);
-                timePickerDialog.show();
-            }
-        });
-
-
     }
 
     @Override
@@ -133,4 +112,88 @@ public class AddMatch extends Fragment {
         binding = null;
     }
 
+    private ArrayList<String> getAvailableTimes() {
+        ArrayList<String> times = new ArrayList<String>();
+        String time;
+
+        for (int i = 0; i < 24; i++) {
+            if (i < 10) {
+                time = "0" + i + ".00";
+            } else {
+                time = i + ".00";
+            }
+            times.add(time);
+        }
+
+        return times;
+    }
+
+    private void initializeSpinnerAdapters() {
+        // Pitch
+        ArrayAdapter<CharSequence> pitchAA = ArrayAdapter.createFromResource(getContext(),
+                R.array.locations_array, android.R.layout.simple_spinner_item);
+        pitchAA.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.locationSpinner.setAdapter(pitchAA);
+        binding.locationSpinner.setOnItemSelectedListener(this);
+
+        // Time
+        ArrayList<String> availableTimes = getAvailableTimes();
+
+        ArrayAdapter<String> timeAA = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, availableTimes);
+        timeAA.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.timeSpinner.setAdapter(timeAA);
+        binding.timeSpinner.setOnItemSelectedListener(this);
+
+        // Position
+        ArrayAdapter<CharSequence> playerCountAA = ArrayAdapter.createFromResource(getContext(),
+                R.array.number_of_players, android.R.layout.simple_spinner_item);
+        playerCountAA.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.playerCountSpinner.setAdapter(playerCountAA);
+        binding.playerCountSpinner.setOnItemSelectedListener(this);
+
+        // Position
+        ArrayAdapter<CharSequence> positionAA = ArrayAdapter.createFromResource(getContext(),
+                R.array.positions_array, android.R.layout.simple_spinner_item);
+        positionAA.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.positionSpinner.setAdapter(positionAA);
+        binding.playerCountSpinner.setOnItemSelectedListener(this);
+    }
+
+    private String getFormattedNumber(int number) {
+        String str;
+        if (number < 10) {
+            str = "0" + number;
+        } else {
+            str = "" + number;
+        }
+
+        return str;
+    }
+
+    private String getFormattedDate(int day, int month, int year) {
+        return getFormattedNumber(day) + "." + getFormattedNumber(month) + "." + getFormattedNumber(year);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        switch (parent.getId()) {
+            case R.id.locationSpinner:
+                location = parent.getSelectedItem().toString();
+                break;
+            case R.id.timeSpinner:
+                time = parent.getSelectedItem().toString();
+                break;
+            case R.id.playerCountSpinner:
+                playerCount = parent.getSelectedItem().toString();
+                break;
+            case R.id.positionSpinner:
+                position = parent.getSelectedItem().toString();
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
