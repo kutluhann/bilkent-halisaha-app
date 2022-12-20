4e5f107e7bb21663d3f5ef4337c30fdfa0da343e
Kaanayden <kaanayden15@gmail.com>
2022-12-20 21:40:07 +0300

    Fix passed match attendance bug

--- app/src/main/java/com/example/bilkenthalisahaapp/Firestore.java
+++ app/src/main/java/com/example/bilkenthalisahaapp/Firestore.java
@@ -62,8 +62,6 @@ public class Firestore {
     public static void addPlayerToMatch(Player player , Match match) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         DocumentReference matchRef = db.collection("matches").document(match.getMatchId());
-        matchRef.update("players", FieldValue.arrayUnion(player));
-        matchRef.update("userIds", FieldValue.arrayUnion(player.getUserID()));
 
         db.runTransaction(new Transaction.Function<Void>() {
             @Override
